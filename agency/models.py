0a7b895d56bdfa2f2b4f6a0c7f2182640000ae8d
Oleh Bohatov <obohatov@gmail.com>
2023-10-01 19:24:24 +0200

    create models, add app to settings, create agency/urls

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic, related_name="newspaper", on_delete=models.SET_NULL, null=True
+    )
+    publishers = models.ManyToManyField("Redactor", related_name="newspapers")
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+    workplace = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-years_of_experience"]
