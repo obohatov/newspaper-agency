69cf46e44518c61312bb888574c62f74d47fd796
Oleh Bohatov <obohatov@gmail.com>
2023-10-02 20:30:01 +0200

    Add forms

--- agency/forms.py
+++ agency/forms.py
@@ -3,3 +3,46 @@ from django.contrib.auth.forms import UserCreationForm
 
 from agency.models import Redactor
 
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by title..."}),
+    )
+
+
+class TopicSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name..."}),
+    )
+
+
+class RedactorForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+
+    class Meta(UserCreationForm):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience",
+            "workplace",
+            "email",
+        )
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by username..."}
+        ),
+    )
