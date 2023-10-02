d9396599c2aa9f47b578dae381cd4f393812f521
Oleh Bohatov <obohatov@gmail.com>
2023-10-02 07:52:51 +0200

    Add urls to agency, create forms and views

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
