9b61318849d43e9d67ca3506d8ce6f94adff63f3
Oleh Bohatov <obohatov@gmail.com>
2023-10-08 22:11:26 +0200

    Fix README and settings of project

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=&60*ah1v=hieb@n)q!cyl_e8eqm=6gk!yjo@d+%65d72t(en$'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
